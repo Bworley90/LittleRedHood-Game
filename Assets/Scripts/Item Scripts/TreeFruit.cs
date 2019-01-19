a65735287036fbf29c023c4d4b609abdaa2e57a9
Bworley90 <40043696+Bworley90@users.noreply.github.com>
2019-01-19 15:54:59 -0500

    Added Orange Bounce Animation
    Commented out the moveto and added an impulse from an array to give it a falling feel

--- Assets/Scripts/Item Scripts/TreeFruit.cs
+++ Assets/Scripts/Item Scripts/TreeFruit.cs
@@ -9,6 +9,8 @@ public class TreeFruit : MonoBehaviour
     public GameObject target;
     public GameObject gameInfo;
     public int fruitMultiplier;
+    private float[] randomDirection = new float[2];
+    private int randomRangeNumber;
     
 
     // Start is called before the first frame update
@@ -19,13 +21,23 @@ public class TreeFruit : MonoBehaviour
         fruitMultiplier = gameInfo.GetComponent<GameInfo>().fruitCountMultiplier;
     }
 
+    private void Awake()
+    {
+        randomDirection[0] = -100; // Force of the fruit
+        randomDirection[1] = 100; // Force of the fruit
+        randomRangeNumber = Random.Range(0, 2); // Grabbing a 1 or 0 to set the direction of the fruit
+        SpawnFruit();
+        StartCoroutine("StopGravity");
+    }
+
     // Update is called once per frame
+    /*
     void FixedUpdate()
     {
         Vector3 PathToPlayer = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
         gameObject.GetComponent<Rigidbody2D>().MovePosition(PathToPlayer);
     }
-
+    */
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Player")) // Does it collide with the player
@@ -33,5 +45,17 @@ public class TreeFruit : MonoBehaviour
             gameInfo.GetComponent<GameInfo>().fruitCount += fruitMultiplier; // fruitMultiplier to fruit counter
             Destroy(gameObject); // Destroying the object;
         }
+    }
+
+    private void SpawnFruit ()
+    {
+        GetComponent<Rigidbody2D>().AddForceAtPosition(new Vector2(randomDirection[randomRangeNumber], 200), transform.position);
+    }
+
+    private IEnumerator StopGravity()
+    {
+        yield return new WaitForSeconds(.6f);
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 0; // turn off gravity;
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0); // Set velocity to 0 to stop movement;
     }
 }
